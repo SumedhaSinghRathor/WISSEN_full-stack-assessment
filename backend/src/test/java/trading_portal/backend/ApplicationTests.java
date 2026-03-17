fff137b1d3f981c02dd3b72558f9ed73ad072bb0
SumedhaSinghRathor <sumedharathor@gmail.com>
2026-03-17 12:10:05 +0530

    backend template

---
+++ backend/src/test/java/trading_portal/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package trading_portal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
